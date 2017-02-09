1793216e9d98807bab2c329d168e67ef62f090cf
Snurnicyn Konstatin <snurnicynkv@gmail.com>
2017-02-09 17:45:35 +0600

    get list of details

--- src/main/java/ru/ks_on_v/controller/CarController.java
+++ src/main/java/ru/ks_on_v/controller/CarController.java
@@ -1,12 +1,15 @@
 package ru.ks_on_v.controller;
 
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.ks_on_v.model.Car;
+import ru.ks_on_v.model.Detail;
 import ru.ks_on_v.service.CarService;
 
 import java.util.List;
@@ -17,9 +20,11 @@ public class CarController {
     @Autowired
     CarService carService;
 
+    @Transactional
     @RequestMapping(value = { "/show-{id}-car" }, method = RequestMethod.GET)
     public String showCat(@PathVariable int id, ModelMap model) {
         Car car = carService.getCar(id);
+        List <Detail> details = car.getDetails();
         model.addAttribute("car", car);
         return "showCar";
     }
