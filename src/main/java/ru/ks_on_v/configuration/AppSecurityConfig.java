6c0504fe9d4e8e8581953a9afc1d9eccec260198
Snurnicyn Konstatin <snurnicynkv@gmail.com>
2017-02-13 15:59:27 +0600

    fix import bug

--- src/main/java/ru/ks_on_v/configuration/AppSecurityConfig.java
+++ src/main/java/ru/ks_on_v/configuration/AppSecurityConfig.java
@@ -2,7 +2,10 @@ package ru.ks_on_v.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
