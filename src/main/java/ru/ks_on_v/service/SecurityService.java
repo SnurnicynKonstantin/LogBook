8e8ecb7b111cc53e15d42a3780ad44e9e4868ae1
Snurnicyn Konstatin <snurnicynkv@gmail.com>
2017-02-15 14:46:42 +0600

    delete login classes

--- src/main/java/ru/ks_on_v/service/SecurityService.java
+++
@@ -1,7 +0,0 @@
-package ru.ks_on_v.service;
-
-public interface SecurityService {
-    public String findLoggedInUsername();
-
-    public void autologin(int id, String password);
-}
--- src/main/java/ru/ks_on_v/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package ru.ks_on_v.service;
-
-import ru.ks_on_v.model.User;
-
-public interface UserService {
-    public void save(User user);
-
-    public User findById(int id);
-}
